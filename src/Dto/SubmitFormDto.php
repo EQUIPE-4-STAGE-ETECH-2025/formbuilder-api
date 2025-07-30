e10f46907d56c7390d779a08ffe129500d7e44db
Salohyy212 <zerambaysalohy@gmail.com>
2025-07-30 15:17:27 +0300

    feat: implement SubmitFormDto with validation

---
+++ src/Dto/SubmitFormDto.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Dto;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class SubmitFormDto
+{
+    /**
+     * @var array<string, mixed>
+     */
+    #[Assert\NotBlank(message: 'Les données du formulaire sont obligatoires')]
+    #[Assert\Type(type: 'array', message: 'Les données doivent être un tableau')]
+    private array $data = [];
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     */
+    public function setData(array $data): void
+    {
+        $this->data = $data;
+    }
+}
