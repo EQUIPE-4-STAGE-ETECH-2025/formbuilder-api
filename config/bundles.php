fb9967f854b114c9b8d899a928d9135634417e5d
Sun-2302 <hei.dera.932@gmail.com>
2025-08-01 13:51:17 +0300

    refactor: rm lexik/jwt-authentication-bundle and change to firebase/php-jwt

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,4 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
