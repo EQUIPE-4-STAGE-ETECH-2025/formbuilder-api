f5b089699054045fd5a0ac6ca6b5ad98f7506e40
Sun-2302 <hei.dera.932@gmail.com>
2025-08-04 12:28:31 +0300

    feat: add BlackListedTokenService

---
+++ src/Service/BlackListedTokenService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use App\Dto\BlackListedTokenDto;
+use App\Entity\BlackListedToken;
+use App\Repository\BlackListedTokenRepository;
+
+class BlackListedTokenService
+{
+    public function __construct(
+        private readonly BlackListedTokenRepository $repository
+    ) {}
+
+    public function blacklist(BlackListedTokenDto $dto): void
+    {
+        $token = (new BlackListedToken())
+            ->setToken($dto->token)
+            ->setExpiresAt($dto->expiresAt);
+
+        $this->repository->save($token);
+    }
+
+    public function isBlacklisted(string $token): bool
+    {
+        return (bool) $this->repository->findOneBy(['token' => $token]);
+    }
+}
