7f90f79de36a663017503c19a65e110ad49bc9ba
Salohyy212 <zerambaysalohy@gmail.com>
2025-08-05 14:39:37 +0300

    feat: implement CreateSubscriptionDto

---
+++ src/Dto/CreateSubscriptionDto.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Dto;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class CreateSubscriptionDto
+{
+    #[Assert\NotBlank]
+    #[Assert\Uuid]
+    public string $planId;
+
+    #[Assert\NotBlank]
+    #[Assert\Email]
+    public string $userEmail;
+
+    public function __construct(array $data)
+    {
+        $this->planId = $data['planId'] ?? '';
+        $this->userEmail = $data['userEmail'] ?? '';
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'planId' => $this->planId,
+            'userEmail' => $this->userEmail,
+        ];
+    }
+}
