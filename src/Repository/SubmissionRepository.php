03f77d5f97ef5b95b96443ca4c73cf860d6e7123
Sun-2302 <hei.dera.932@gmail.com>
2025-08-13 11:17:55 +0300

    feat: add countByUserForms, countSubmissionsPerMonthByUser and countSubmissionsPerFormByUser in SubmissionRepository

--- src/Repository/SubmissionRepository.php
+++ src/Repository/SubmissionRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Submission;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\DBAL\Exception;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -38,4 +39,51 @@ class SubmissionRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
+
+    public function countByUserForms(string $userId): int
+    {
+        $qb = $this->createQueryBuilder('s')
+            ->select('COUNT(s.id)')
+            ->join('s.form', 'f')
+            ->where('f.user = :userId')
+            ->setParameter('userId', $userId);
+
+        return (int) $qb->getQuery()->getSingleScalarResult();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function countSubmissionsPerMonthByUser(string $userId): array
+    {
+        $sql = "
+        SELECT to_char(s.submitted_at, 'YYYY-MM') AS month, COUNT(s.id) AS submissionsCount
+        FROM submission s
+        INNER JOIN form f ON s.form_id = f.id
+        WHERE f.user_id = :userId
+        GROUP BY month
+        ORDER BY month ASC
+    ";
+
+        return $this->getEntityManager()->getConnection()
+            ->executeQuery($sql, ['userId' => $userId])
+            ->fetchAllKeyValue();
+    }
+
+    public function countSubmissionsPerFormByUser(string $userId): array
+    {
+        $sql = "
+        SELECT f.title AS formTitle, COUNT(s.id) AS submissionsCount
+        FROM submission s
+        INNER JOIN form f ON s.form_id = f.id
+        WHERE f.user_id = :userId
+        GROUP BY f.title
+        ORDER BY submissionsCount DESC
+    ";
+
+        return $this->getEntityManager()->getConnection()
+            ->executeQuery($sql, ['userId' => $userId])
+            ->fetchAllKeyValue();
+    }
+
 }
