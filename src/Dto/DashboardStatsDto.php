cc6abf84020e474801eef311cb95f1682cd5b7d5
Sun-2302 <hei.dera.932@gmail.com>
2025-08-13 11:15:44 +0300

    feat: add DashboardStatsDto

---
+++ src/Dto/DashboardStatsDto.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Dto;
+
+class DashboardStatsDto
+{
+    private int $totalForms;
+    private int $publishedForms;
+    private int $totalSubmissions;
+
+    /** @var array<string,int> nombre de soumissions par mois */
+    private array $submissionsPerMonth;
+
+    /** @var array<string,int> nombre de soumissions par formulaire */
+    private array $submissionsPerForm;
+
+    /** @var array<string,int> nombre de formulaires par statut */
+    private array $formsStatusCount;
+
+    public function __construct(
+        int $totalForms,
+        int $publishedForms,
+        int $totalSubmissions,
+        array $submissionsPerMonth = [],
+        array $submissionsPerForm = [],
+        array $formsStatusCount = []
+    ) {
+        $this->totalForms = $totalForms;
+        $this->publishedForms = $publishedForms;
+        $this->totalSubmissions = $totalSubmissions;
+        $this->submissionsPerMonth = $submissionsPerMonth;
+        $this->submissionsPerForm = $submissionsPerForm;
+        $this->formsStatusCount = $formsStatusCount;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'totalForms' => $this->totalForms,
+            'publishedForms' => $this->publishedForms,
+            'totalSubmissions' => $this->totalSubmissions,
+            'submissionsPerMonth' => $this->submissionsPerMonth,
+            'submissionsPerForm' => $this->submissionsPerForm,
+            'formsStatusCount' => $this->formsStatusCount,
+        ];
+    }
+}
